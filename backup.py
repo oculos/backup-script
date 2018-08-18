b605064f5f94c3c8918e1d2b67c6b44d86465d4e
Francis Augusto Medeiros-Logeay <register@francisaugusto.com>
2018-08-18 15:49:03 +0000

    fixed

--- backup.py
+++ backup.py
@@ -8,51 +8,55 @@ jobs = {}
 destinations = {}
 log_data = []
 settings = {}
-path_temp = ""
-
+path_temp = "dummy"
 def run_jobs():
 	global destinations,path_temp
-    
-    # cleanup 
-    if clean_up()== 0:
-        log("INFO","admin","Cleaning up temp directory.") 
-        
+	
+	path_temp = settings['temp'].strip()
+	if path_temp == "":
+		print ("Temp directory blank. Please define a temporary directory on settings.json")
+		log("FAULT","admin","Temp directory blank. Script can't run without a temp directory.")
+	if path_temp[-1] != '/':
+		path_temp = path_temp+'/'
+
+	# cleanup
+	if clean_up()== 0:
+		log("INFO","admin","Cleaning up temp directory.")
+	
 	for job in jobs:
 		job_name = job['name']
-		log("INFO", job_name,"Starting backup.") 
+		log("INFO", job_name,"Starting backup.")
 		destiny = return_array(job['destinations'])
 		if len(destiny)==0:
 			log("WARN",job_name,"Job has no destination, skipping...")
-			continue 
-		path_temp = settings['temp'].strip() 
-		if path_temp[-1] != '/':
-			path_temp = path_temp+'/'
+			continue
 		
 		# compress paths
 		if "path" in job:
-            compress_paths(job)	
-			
+			compress_paths(job)
+		
 		
 		# Process docker
 		if "docker" in job:
-            compress_docker(job)
+			compress_docker(job)
 		
-        # upload compressed files to backup servers
-        upload_files(job)
-        	
+		# upload compressed files to backup servers
+		upload_files(job)
+		
 		# cleanup
 		if clean_up() == 0:
-            log("INFO",job_name,"All cleaned up, job finished.") 	
-
+			log("INFO",job_name,"All cleaned up, job finished.")
 					
+
 def compress_paths(job):
+	job_name = job['name']
 	paths = return_array(job["path"])
 	for path in paths:
 		log("INFO",job_name,"Compressing "+path)
-		path_stamp = path.replace('/','-') 
-	
-		p,output,err = exec_command(['tar','cpfz',path_temp+job_name+path_stamp+'-'+datestamp()+'.tar.gz',path],False) 
-		if p ==1: 
+		path_stamp = path.replace('/','-')
+		
+		p,output,err = exec_command(['tar','cpfz',path_temp+job_name+path_stamp+'-'+datestamp()+'.tar.gz',path],False)
+		if p ==1:
 			log("WARN",job_name,"Exit "+str(p)+" - "+err)
 		elif p==0:
 			log("INFO",job_name,"Compression successfull.")
@@ -60,10 +64,11 @@ def compress_paths(job):
 			log("FAULT",job_name,"Exit "+str(p)+" - "+err);
 
 def compress_docker(job):
-	dockers = return_array(job["docker"]) 
+	job_name = job['name']
+	dockers = return_array(job["docker"])
 	for docker in dockers:
 		container = ""
-
+		
 		# get the container name
 		p,output,err = exec_command(["docker", "inspect", "--format", "'{{.Name}}'",docker], False)
 		if p == 0:
@@ -71,14 +76,14 @@ def compress_docker(job):
 			log("INFO",job_name,"Starting dumping of docker container "+container)
 		else:
 			log("WARN",job_name,"Container "+docker+" doesn't seem to exist. Skipping...")
-			continue 
-
+			continue
+		
 		# save the container as an image
 		log("INFO",job_name,"Creating image of container "+container)
 		dest_cont = job_name+"-docker-"+container+"-"+datestamp()
 		p,output,err = exec_command(["docker","commit","-p",docker,dest_cont],False)
 		if p != 0:
-			log("WARN",job_name,"Container "+container+" couldn't be saved as an image.") 
+			log("WARN",job_name,"Container "+container+" couldn't be saved as an image.")
 			continue
 		
 		# compress docker image
@@ -86,32 +91,33 @@ def compress_docker(job):
 		log("INFO",job_name,"Compressing image.")
 		tar_docker = ["docker","save", dest_cont]
 		gzip_docker = ["gzip",">",path_temp+dest_cont+".tar.gz"]
-		try: 
+		try:
 			p = subprocess.check_output("docker save "+dest_cont+" | gzip > "+path_temp+dest_cont+".tar.gz",shell=True)
 		except subprocess.CalledProcessError as grepexc:
-			if grepexc.returncode == 0: 	
+			if grepexc.returncode == 0:
 				log("INFO",job_name,"Docker container "+container+" successfully compressed.")
 			else:
 				log("FAULT",job_name,"Compressing the container "+container+" has failed.")
-
+		
 		# erase image
 		p,output,err = exec_command(["docker","rmi",dest_cont],False)
 		if p == 0:
 			log("INFO",job_name,"Image of container "+container+" was successfully removed.")
 		else:
 			log("FAULT",job_name,"Image of container "+container+" wasn't removed!")
-        
+
 
 def upload_files(job):
-    destiny = return_array(job['destinations'])
+	job_name = job['name']
+	destiny = return_array(job['destinations'])
 	# transfer files to remote servers
 	for destination in destiny:
 		dest = {}
 		for d in destinations:
 			if d["name"] == destination:
 				dest = d
-				log("INFO",job_name,"Starting to upload files to remote server: "+dest["address"]) 
-				command = ["scp"] 
+				log("INFO",job_name,"Starting to upload files to remote server: "+dest["address"])
+				command = ["scp"]
 				if "key" in dest:
 					command.extend(["-i",dest["key"]])
 				if "port" in dest:
@@ -122,7 +128,7 @@ def upload_files(job):
 				try:
 					p = subprocess.check_output(" ".join(command),shell = True)
 				except subprocess.CalledProcessError as grepexc:
-                                    	if grepexc.returncode == 0:
+					if grepexc.returncode == 0:
 						log("INFO",job_name,"Files transferred successfully! Cleaning up...")
 					else:
 						log("FAULT",job_name,"Transfer has failed! check your settings: "+p)
@@ -133,15 +139,15 @@ def clean_up():
 		return 0
 	except subprocess.CalledProcessError as grepexc:
 		if grepexc.returncode != 0:
-			log("FAULT",job_name,"Failed to clean up: "+grepexc.output)    
-            return grepexc.returncode
+			log("FAULT",job_name,"Failed to clean up: "+grepexc.output)
+			return grepexc.returncode
 
 def exec_command(command, shell):
-	p = subprocess.Popen(command, shell,stdout=subprocess.PIPE, stderr = subprocess.PIPE)		
+	p = subprocess.Popen(command, shell,stdout=subprocess.PIPE, stderr = subprocess.PIPE)
 	output,err = p.communicate()
 	return p.returncode,output,err
 
-def return_array(obj): 
+def return_array(obj):
 	if isinstance(obj,list):
 		return obj
 	else:
@@ -158,13 +164,13 @@ def log(level,job,message):
 
 def datestamp():
 	fulltime = str(datetime.now())
-	partialtime = ""+fulltime[0:10]+"-"+fulltime[11:16].replace(':','-') 
+	partialtime = ""+fulltime[0:10]+"-"+fulltime[11:16].replace(':','-')
 	return partialtime
 
-def read_jobs(): 
+def read_jobs():
 	global jobs
 	with open("servers.json","r") as f:
-		jobs = json.load(f)	
+		jobs = json.load(f)
 
 def read_destinations():
 	global destinations
@@ -182,3 +188,4 @@ read_jobs()
 read_destinations()
 read_settings()
 run_jobs()
+
